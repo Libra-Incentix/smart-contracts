c54d3823f871cba1eebba0cff51f590d420c52e1
Mubcy <65167470+Mubcy@users.noreply.github.com>
2022-04-21 08:40:26 +0100

    initial commit

---
+++ token-airdrop/contracts/Token.sol
@@ -0,0 +1,7 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.7.0;
+
+import '@openzeppelin/contracts/token/ERC20/ERC20.sol';
+
+// Inherenting ERC20 basic standard
+abstract contract Token is ERC20 {}
---
+++ token-airdrop/hardhat.config.js
@@ -0,0 +1,21 @@
+require("@nomiclabs/hardhat-waffle");
+
+const { privateKey } = require('./secrets.json');
+
+module.exports = {
+  solidity: "0.8.0",
+  networks: {
+    testnet: {
+      url: "https://data-seed-prebsc-1-s1.binance.org:8545",
+      chainId: 97,
+      gasPrice: 20000000000,
+      accounts: [privateKey]
+    },
+    mainnet: {
+      url: "https://bsc-dataseed.binance.org/",
+      chainId: 56,
+      gasPrice: 20000000000,
+      accounts: [privateKey]
+    }
+  },
+};
