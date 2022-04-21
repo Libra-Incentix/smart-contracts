b19ad18df38999dd850496c51b27d2e077419ecb
Mubcy <65167470+Mubcy@users.noreply.github.com>
2022-04-21 08:40:57 +0100

    Delete token-airdrop/contracts directory

--- token-airdrop/contracts/Token.sol
+++
@@ -1,7 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity >=0.7.0;
-
-import '@openzeppelin/contracts/token/ERC20/ERC20.sol';
-
-// Inherenting ERC20 basic standard
-abstract contract Token is ERC20 {}
