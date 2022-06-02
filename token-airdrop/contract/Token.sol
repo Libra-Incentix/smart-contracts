711ef9b9bf0ab26c400e8d276711a6879227e73e
Mubcy <65167470+Mubcy@users.noreply.github.com>
2022-06-02 20:34:20 +0100

    Update Token.sol

--- token-airdrop/contract/Token.sol
+++ token-airdrop/contract/Token.sol
@@ -4,7 +4,6 @@ pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
-
 // Inherenting ERC20 basic standard
 contract Token is ERC20, Ownable {
 	constructor()ERC20("Test Token","TTT"){}
